31734aa1a11ee357e2156f8cf9e9718de8fe82d3
Mohtasheem <mohtasheemejaz@gmail.com>
2022-05-05 17:49:55 +0530

    Dynamic Page-1

--- pages/[id].js
+++ pages/[id].js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
 const NewsTitle = () => {
+
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        fetch(`https://newsapi.org/v2/everything?q=cnn.com&language=en&apiKey=f688b40cbae84bbb9364589714dc2da6`).then((res) => res.json())
+            .then((json) => {
+                setData(json.articles)
+                console.log(json.articles)
+            })
+    }, [])
+
+    console.log("ggg : - "+data)
+
     return ( 
         <div>
             <Navbar />
